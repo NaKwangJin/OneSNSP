c153c173b5b1879aba3d666cefe5186392f26d47
Chan Yeong Yang <ns3029800@naver.com>
2019-04-03 11:56:51 +0900

    추가됨: 로그인 & 회원가입 엑티비티

---
+++ app/src/main/java/com/example/onesns/LoginActivity.java
@@ -0,0 +1,40 @@
+package com.example.onesns;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button gotoRegisterBtn;
+    private Button gotoLoginBtn;
+    private Context cont;
+
+    private void InitComponents(){
+        gotoRegisterBtn = (Button)findViewById(R.id.regbtn);
+        gotoLoginBtn = (Button)findViewById(R.id.loginbtn);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        cont = this;
+
+        InitComponents();
+
+        gotoRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(cont,RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
